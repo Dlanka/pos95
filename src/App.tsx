fd0ea01e6d072fcb5870a2c295336ad3921c8123
Dlanka <darshaka4mail@gamil.com>
2024-06-16 22:57:11 +0530

    Add base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 function App() {
   return (
     <>
-      <div className="flex  items-center justify-center">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="flex flex-col h-[calc(100vh-theme(spacing.16))] mt-16 ml-[240px]">
+        <header className="flex items-center w-full h-16 pl-4 pr-4 fixed top-0 left-0 ">
+          head
+        </header>
+
+        <aside className="w-[240px] h-[calc(100vh-theme(spacing.16))] top-16 left-0 fixed "></aside>
+
+        <main className="flex-1 flex h-full overflow-hidden p-4 pt-0">
+          <div className="rounded-2xl bg-white h-full overflow-y-auto w-full">
+            <div style={{ height: "1200px" }}></div>
+          </div>
+        </main>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#fff',
+      black: '#000',
+      primary: {
+        50: "#f7f7f7",
+        100: "#f2f2f2",
+        200: "#e8e8e8",
+        300: "#cfcfcf",
+        400: "#8f8f8f",
+        500: "#6d6d6d",
+        600: "#5d5d5d",
+        700: "#4f4f4f",
+        800: "#454545",
+        900: "#3d3d3d",
+        950: "#000",
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
